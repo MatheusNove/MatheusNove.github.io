e5e253e9762462bdcabf7e008b13650c7abe0aa4
Matheus Novelli <matheusnove@hotmail.com>
2026-08-17 23:18:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,51 @@
 const projectCards = document.querySelectorAll(".project-card");
 
 projectCards.forEach(card => {
-    
+
+    // Get the images belonging to THIS card
     const images = card.dataset.images.split(",");
+
     let currentImage = 0;
 
-const imageElement =
-    document.getElementById("console-image");
-
+    // Find the elements belonging to THIS card
     const imageElement = card.querySelector(".project-media img");
     const nextButton = card.querySelector(".next");
     const prevButton = card.querySelector(".prev");
 
 
-    
-    function UpdateImage(){
+    function updateImage() {
+
         imageElement.style.opacity = 0;
-    
+
         setTimeout(() => {
-    
+
             imageElement.src = images[currentImage];
-    
+
             imageElement.style.opacity = 1;
-    
+
         }, 300);
     }
 
 
-    
-    /*Button Next*/
-    nextButton.addEventListener("click", () => {    
+    nextButton.addEventListener("click", () => {
         currentImage++;
-    
-        if(currentImage >= images.length){
+
+        if (currentImage >= images.length) {
             currentImage = 0;
         }
-    
-       UpdateImage();
+
+        updateImage();
     });
 
-    /*Button Prev*/
-    prevButton.addEventListener("click", () => {    
+
+    prevButton.addEventListener("click", () => {
         currentImage--;
-    
-        if(currentImage < 0){
-            currentImage =
-                images.length - 1;
+
+        if (currentImage < 0) {
+            currentImage = images.length - 1;
         }
-    
-       UpdateImage();
+
+        updateImage();
     });
 
+});
